ce8fda412f767b954075542c2171cebd95926867
LexaRbn <alexai90@yandex.ru>
2024-07-10 17:16:47 +0300

    code chages

--- program2.cs
+++ program2.cs
@@ -8,11 +8,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
@@ -37,12 +37,6 @@ app.MapGet("/weatherforecast", () =>
 .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
